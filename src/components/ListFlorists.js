d57e33729a54c05348146928432cd3514b9df414
Wendy Calderon <wendycalderon@gmail.com>
2019-11-23 14:56:02 -0500

    Add and render renderFloristsList function

--- src/components/ListFlorists.js
+++ src/components/ListFlorists.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import FloristCard from './FloristCard';
 
-class ListFlorists extends React.Component {
-
-    render(){
-        return(null)
+const ListFlorists = (props) => {
+    const renderFloristsList = () => {
+        const floristCards = props.floristsList.length > 0 ? props.floristsList.map(f => <FloristCard florist={f} key={f.id}/>) : null
+        return (floristCards)
     }
-
+    
+    return (  
+        <div>  
+            <ul>
+                {renderFloristsList()}
+            </ul>
+        </div> 
+    )
 }
 
 export default ListFlorists
