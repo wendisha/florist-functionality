984b4112d1e83e199889fe79b54be2c0558e4c2d
Wendy Calderon <wendycalderon@gmail.com>
2019-11-23 20:47:00 -0500

    Add handleClick funct + Delete unnecessary code

--- src/components/Geolocate.js
+++ src/components/Geolocate.js
@@ -2,24 +2,18 @@ import React from "react";
 import { geolocated } from "react-geolocated";
  
 class Geolocate extends React.Component {
+    handleClick = (event) => {
+        event.preventDefault()
+        this.props.fetchYelpApi(this.props.coords.latitude, this.props.coords.longitude)
+    }
+
     render() {
         return !this.props.isGeolocationAvailable ? (
             <div>Your browser does not support Geolocation</div>
         ) : !this.props.isGeolocationEnabled ? (
             <div>Geolocation is not enabled</div>
         ) : this.props.coords ? (
-            <table>
-                <tbody>
-                    <tr>
-                        <td>latitude</td>
-                        <td>{this.props.coords.latitude}</td>
-                    </tr>
-                    <tr>
-                        <td>longitude</td>
-                        <td>{this.props.coords.longitude}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <button onClick={ this.handleClick }>Browse based on my location</button>
         ) : (
             <div>Getting the location data&hellip; </div>
         );
