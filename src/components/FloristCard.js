8f34606f6620265678b4ac999798f422177b090c
Wendy Calderon <wendycalderon@gmail.com>
2019-11-24 00:12:38 -0500

    Turn link into button + delete unnecessary code

--- src/components/FloristCard.js
+++ src/components/FloristCard.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import CardDeck from "react-bootstrap/CardDeck";
 import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const FloristCard = (props) => {
   return ( 
-    <CardDeck className="col-md-6 col-lg-4">
+    <CardDeck className="col-md-6 col-lg-3" style={{ margin: "2em auto" }}>
       <Card>
       {/* <div key={props.florist.id}> */}
-        <Card.Img variant="top" src={props.florist.image_url} alt={`Florist: ${props.florist.name}`}/>
+        <Card.Img variant="top" className="card-img-top" src={props.florist.image_url} alt={`Florist: ${props.florist.name}`}/>
         <Card.Body>
           <Card.Title>{props.florist.name}</Card.Title>
           <Card.Text>{props.florist.location.address1}</Card.Text>
           <Card.Text>{props.florist.location.city}, {props.florist.location.state}</Card.Text>
           <Card.Text>{props.florist.display_phone}</Card.Text>
-          <Card.Link target="_blank" href={props.florist.url}>Learn more about this florist!</Card.Link>
+          {/* <Card.Link target="_blank" href={props.florist.url}>Learn more about this florist!</Card.Link>
+           */}
+
+          <Button>
+            <Card.Link target="_blank" href={props.florist.url}>
+              Learn more...
+            </Card.Link>
+          </Button>
+
         </Card.Body>
       </Card>
     </CardDeck>
