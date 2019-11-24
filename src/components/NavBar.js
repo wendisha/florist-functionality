30032e7acd58f0b782e7ff31ed63b143aa2ef571
Wendy Calderon <wendycalderon@gmail.com>
2019-11-24 16:31:02 -0500

    Add styling to Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,24 @@
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
 
 const NavBar = () => {
     return(
-        <Navbar variant="dark">
-            <Navbar.Brand href="https://www.lantern.co/">
-                <img src="https://mvp-lantern-staging.s3.us-east-2.amazonaws.com/LOGO_Black.png"
-                width="200" className="d-inline-block align-top" alt="Lantern logo"/>
-            </Navbar.Brand>
-
-
-
-                {/* <a class="max-w-xs" href="/">
-            <img src="https://mvp-lantern-staging.s3.us-east-2.amazonaws.com/LOGO_Black.png" width="200" alt="Lantern">
-          </a> */}
+        // <Container bg="light" expand="lg"  >
+        <Navbar bg="light" variant="dark" style={{ height: '8rem', paddingRight: '15px' }}>
+            <div className="container">
+                <Navbar.Brand href="https://www.lantern.co/">
+                    <img src="https://mvp-lantern-staging.s3.us-east-2.amazonaws.com/LOGO_Black.png"
+                    width="200" className="d-inline-block align-top" alt="Lantern logo"/>
+                </Navbar.Brand>
                 <Nav className="mr-auto">
-                    {/* <NavLink exact activeClassName="active" to="/bookmarks" style={{ color: '#FFF', paddingRight: '15px' }}>Bookmarks</NavLink>
-                    <NavLink exact activeClassName="active" to="/browse-schools" style={{ color: '#FFF' }}>Browse Schools</NavLink> */}
-                    <Nav.Link className="inline-block nav-link text-center" href="https://www.lantern.co/about" style={{ color: '#000', paddingRight: '50px' }}>About Us</Nav.Link>
+                    <Nav.Link className="nav-link text-center" href="https://www.lantern.co/about" style={{ paddingRight: '50px' }}>About Us</Nav.Link>
                     <Nav.Link href="https://www.lantern.co/howitworks" style={{ color: '#000', paddingRight: '50px' }}>How it works</Nav.Link>
                     <Nav.Link href="https://www.lantern.co/articles" style={{ color: '#000', paddingRight: '50px' }}>Articles</Nav.Link>
                 </Nav>
-                {/* { loggedIn ? <Logout/> : null } */}
+            </div>
         </Navbar>
+        // </Container>
     )
 }
-
-{/* <a class="inline-block hover:text-sunriseYellow mx-4 text-center" href="https://www.lantern.co/about">
-About Us
-</a>
-<a class="inline-block hover:text-sunriseYellow mr-4 text-center" href="https://www.lantern.co/howitworks">
-How it works
-</a>
-<a class="inline-block hover:text-sunriseYellow text-center" href="https://www.lantern.co/articles">
-Articles
-</a> */}
-
 
 export default NavBar
