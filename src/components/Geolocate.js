ed182d6645822d7fed4e356d61d79054c273f847
Wendy Calderon <wendycalderon@gmail.com>
2019-11-24 18:06:26 -0500

    Turn button into image and add style

--- src/components/Geolocate.js
+++ src/components/Geolocate.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { geolocated } from "react-geolocated";
+import Button from 'react-bootstrap/Button';
  
 class Geolocate extends React.Component {
     handleClick = (event) => {
@@ -12,8 +13,8 @@ class Geolocate extends React.Component {
             <div>Your browser does not support Geolocation</div>
         ) : !this.props.isGeolocationEnabled ? (
             <div>Geolocation is not enabled</div>
-        ) : this.props.coords ? (
-            <button onClick={ this.handleClick }>Browse based on my location</button>
+        ) : this.props.coords ? ( 
+            <Button variant="light"><img src='https://i.imgur.com/c86tlDi.jpg' onClick={ this.handleClick } /></Button> 
         ) : (
             <div>Getting the location data&hellip; </div>
         );
