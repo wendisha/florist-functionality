39b0a1a660b36fff761c3071a72e6b82aeaae914
Wendy Calderon <wendycalderon@gmail.com>
2019-11-24 14:50:14 -0500

    Change no-image src

--- src/components/FloristCard.js
+++ src/components/FloristCard.js
@@ -4,11 +4,17 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 const FloristCard = (props) => {
+  // console.log(props.florist.image_url);
   return ( 
     <CardDeck className="col-md-6 col-lg-3" style={{ margin: "2em auto" }}>
       <Card>
       {/* <div key={props.florist.id}> */}
-        <Card.Img variant="top" className="card-img-top" src={props.florist.image_url} alt={`Florist: ${props.florist.name}`}/>
+      
+        {/* <Card.Img variant="top" className="card-img-top" src={props.florist.image_url} alt={`Florist: ${props.florist.name}`}/> */}
+        {/* <img src="image.png" onError="this.onerror=null; this.src='/images/noimage.gif';" /> */}
+        {/* src='../public/no-photo.jpg' */}
+        <Card.Img variant="top" className="card-img-top" src={props.florist.image_url} alt={`Florist: ${props.florist.name}`} onError={(e)=>{e.target.onerror = null; e.target.src="https://i.imgur.com/DtxazjC.jpg"}}/>
+                                                                                                                            
         <Card.Body>
           <Card.Title>{props.florist.name}</Card.Title>
           <Card.Text>{props.florist.location.address1}</Card.Text>
