6f8afa95f47b6036136d357b6f3832aa5269535c
Wendy Calderon <wendycalderon@gmail.com>
2019-11-25 00:37:54 -0500

    Updates

---
+++ precache-manifest.0a12e19a95d5a6b59e9744f231d41c40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dd34ea218d98e8726c96ac3283a0fa",
+    "url": "/wendisha/florist-near-me/index.html"
+  },
+  {
+    "revision": "2c1e32c40a1ece491dbf",
+    "url": "/wendisha/florist-near-me/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "415265727adf024c88d1",
+    "url": "/wendisha/florist-near-me/static/css/main.8b499583.chunk.css"
+  },
+  {
+    "revision": "2c1e32c40a1ece491dbf",
+    "url": "/wendisha/florist-near-me/static/js/2.7a21bec5.chunk.js"
+  },
+  {
+    "revision": "415265727adf024c88d1",
+    "url": "/wendisha/florist-near-me/static/js/main.4583c439.chunk.js"
+  },
+  {
+    "revision": "505debd8c8e36b7080ed",
+    "url": "/wendisha/florist-near-me/static/js/runtime-main.c0c528a2.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wendisha/florist-near-me/precache-manifest.0a12e19a95d5a6b59e9744f231d41c40.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wendisha/florist-near-me/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
