1a7541784facf32d838afb5e1a56e2b6b096d234
Wendy Calderon <wendycalderon@gmail.com>
2019-11-24 22:19:35 -0500

    Add hover effect, change text padding, format card

--- src/components/FloristCard.js
+++ src/components/FloristCard.js
@@ -6,8 +6,8 @@ import Button from 'react-bootstrap/Button';
 const FloristCard = (props) => {
   // console.log(props.florist.image_url);
   return ( 
-    <CardDeck className="col-md-6 col-lg-4" style={{ margin: "2em auto" }}>
-      <Card>
+    <CardDeck className="col-md-5 col-lg-4" style={{ margin: "2em auto" }}>
+      <Card className="card">
       {/* <div key={props.florist.id}> */}
       
         {/* <Card.Img variant="top" className="card-img-top" src={props.florist.image_url} alt={`Florist: ${props.florist.name}`}/> */}
@@ -17,15 +17,15 @@ const FloristCard = (props) => {
                                                                                                                             
         <Card.Body>
           <Card.Title>{props.florist.name}</Card.Title>
-          <Card.Text>{props.florist.location.address1}</Card.Text>
-          <Card.Text>{props.florist.location.city}, {props.florist.location.state}</Card.Text>
+          <Card.Text className='cd-text'>{props.florist.location.address1}</Card.Text>
+          <Card.Text className='cd-text'>{props.florist.location.city}, {props.florist.location.state}</Card.Text>
           <Card.Text>{props.florist.display_phone}</Card.Text>
           {/* <Card.Link target="_blank" href={props.florist.url}>Learn more about this florist!</Card.Link>
            */}
 
           <Button className="align-self-end btn-md round btn-block" variant="warning" style={{ margin: "auto" }}>
             <Card.Link target="_blank" href={props.florist.url} style={{ color: '#000' }}>
-              Learn more...
+              Learn More
             </Card.Link>
           </Button>
 
