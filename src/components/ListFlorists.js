c2685038de73e48a17cffc82102893452c4c5f8d
Wendy Calderon <wendycalderon@gmail.com>
2019-11-25 01:48:48 -0500

    Add comment

--- src/components/ListFlorists.js
+++ src/components/ListFlorists.js
@@ -2,6 +2,7 @@ import React from 'react'
 import FloristCard from './FloristCard';
 
 const ListFlorists = (props) => {
+    //Map through florist objects 
     const renderFloristsList = () => {
         const floristCards = props.floristsList.length > 0 ? props.floristsList.map(f => <FloristCard florist={f} key={f.id}/>) : null
         return (floristCards)
